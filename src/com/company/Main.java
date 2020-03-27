339253cd330c3b1e3e0655efc48423e674a495eb
tom.pradella@gmail.com <noR8Gh&tb?+Qfh5K2)+wyp8H>
2020-03-27 19:50:21 +0100

    Die Bearbeitung beginnt nächste Woche.

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Ich beginne mir der Bearbeitung nächste Woche, da ich die letzten zwei Wochen eine Menge zu tun hatte.
+    }
+}
